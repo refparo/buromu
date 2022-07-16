f05c3423fec66303c43891604c2c4f0d5e5a3c46
refparo <ref.paro@outlook.com>
2022-07-16 17:45:38 +0800

    feature: improve the conversation logic of task management also upgrade gradle

--- src/main/kotlin/one/paro/buromu/Alarm.kt
+++ src/main/kotlin/one/paro/buromu/Alarm.kt
@@ -176,7 +176,7 @@ object Alarm: CoroutineScope by Main {
         recordPastActivity(subject, now - 1, activity, now)
       }
       suspend fun taskToday(
-        subject: Friend, length: Int, task: String, noDisturb: Boolean
+        subject: Friend, length: Int, task: String
       ) {
         val data = Data[subject] ?: return
         val now = LocalTime.now().hour
@@ -185,30 +185,28 @@ object Alarm: CoroutineScope by Main {
           subject.sendMessage("等到了工作时间再说吧")
           return
         }
-        data.tasks.addAll(task.split(',', '，', '、').filter { it.isNotEmpty() })
-        if (noDisturb) {
-          data.noDisturb = true
-          if (now + length >= data.endOfDay) {
-            subject.sendMessage("要那么长时间吗")
-            subject.sendMessage("那我今天都不打扰你了")
-          } else {
-            launch {
-              delay(length.hours.inWholeMilliseconds)
-              data.noDisturb = false
-            }
-            subject.sendMessage("明白了")
-          }
+        (now until now + length).forEach {
+          data.activities[it] = task
+        }
+        data.noDisturb = true
+        if (now + length >= data.endOfDay) {
+          subject.sendMessage("要那么长时间吗")
+          subject.sendMessage("那我今天都不打扰你了")
         } else {
-          subject.sendMessage("明白了，我会提醒你的")
+          launch {
+            delay(length.hours.inWholeMilliseconds)
+            data.noDisturb = false
+          }
+          subject.sendMessage("明白了")
         }
       }
       matching(Regex("等会我?[要会](.+?)([ ,，]*别打扰我)?")) {
-        val (task, noDisturb) = it.destructured
-        taskToday(subject, 1, task, noDisturb != "")
+        val (task) = it.destructured
+        taskToday(subject, 1, task)
       }
       matching(Regex("""之后(\d{1,2})小时我?[要会](.+?)([ ,，]*别打扰我)?""")) {
-        val (length, task, noDisturb) = it.destructured
-        taskToday(subject, length.toInt(), task, noDisturb != "")
+        val (length, task) = it.destructured
+        taskToday(subject, length.toInt(), task)
       }
       suspend fun taskFuture(
         subject: Friend, date: LocalDate, task: String, noDisturb: Boolean
@@ -218,14 +216,24 @@ object Alarm: CoroutineScope by Main {
           subject.sendMessage("但是那一天已经过去了啊……你不会穿越了吧？")
           return
         }
-        data.calendar[date.toString()] = UserDay(
-          noDisturb,
-          (data.calendar[date.toString()]?.tasks ?: setOf()) +
-            task.split(',', '，', '、').filter { it.isNotEmpty() },
-          mapOf(),
-          null
-        )
-        subject.sendMessage("明白了，我会提醒你的")
+        if (noDisturb) {
+          data.calendar[date.toString()] = UserDay(
+            true,
+            setOf(),
+            mapOf(Pair(data.endOfDay - 1, task)),
+            null
+          )
+          subject.sendMessage("明白了")
+        } else {
+          data.calendar[date.toString()] = UserDay(
+            false,
+            (data.calendar[date.toString()]?.tasks ?: setOf()) +
+              task.split(',', '，', '、').filter { it.isNotEmpty() },
+            mapOf(),
+            null
+          )
+          subject.sendMessage("明白了，我会提醒你的")
+        }
       }
       matching(Regex("([今明后])天我?(整天都)?[要会](.+)")) {
         val (relDate, noDisturb, task) = it.destructured
@@ -325,65 +333,106 @@ object Alarm: CoroutineScope by Main {
         }
         in data.startOfDay until data.endOfDay -> {
           if (data.noDisturb) return@launch
-          subject.sendMessageIn(
-            "又一个小时过去了",
-            "${now}点到了！"
-          )
-          subject.sendMessageIn(
-            "这一小时做了什么呢",
-            "在做什么呢？"
-          )
-          select<Unit> {
-            nextMessageAsync(subject).onAwait {
-              val msg = it.message.contentToString().trimStart('在')
-              data.activities[now - 1] = msg
-              val doneTasks = data.tasks.filter { msg.contains(it) }
-                .onEach { data.tasks.remove(it) }
-              if (doneTasks.isNotEmpty()) {
-                subject.sendMessage("不错嘛，完成了${doneTasks.size}项目标")
-                subject.sendMessageIn(
-                  "休息一下再继续吧",
-                  "继续之前，给自己一点放松时间吧"
-                )
-                if (data.tasks.isNotEmpty())
-                  subject.sendMessage("接下来还要${data.tasks.joinToString("、")}")
-                else
-                  subject.sendMessage("这样今天的目标就全都完成啦")
-              } else when {
-                msg.contains(Regex("什么都没做|什么也没做|摸鱼")) -> {
-                  data.activities[now - 1] = "摸鱼"
-                  subject.sendMessageIn("啊这", "这样啊")
-                  subject.sendMessage("那接下来得加油咯")
-                }
-                msg.contains(Regex("色色|打飞机|撸管|手冲")) -> {
-                  subject.sendMessageIn("啧", "嘶")
-                  subject.sendMessage("那不打扰你了")
-                  subject.sendMessage("玩得愉快hhh")
-                }
-                msg.contains(Regex("坐车|开车")) -> {
-                  subject.sendMessage("赶路很辛苦吧")
-                  subject.sendMessage("祝你不虚此行")
-                }
-                msg.contains(Regex("午休|休息")) -> {
-                  subject.sendMessage("休息得好吗？")
-                  subject.sendMessage("恢复精力之后就继续工作吧")
-                }
-                msg.contains(Regex("[玩打](游戏|.游)|出去玩")) -> {
-                  subject.sendMessage("那就……玩得开心")
-                }
-                else -> {
-                  subject.sendMessageIn("不错嘛", "可以")
+          if (
+            data.activities.containsKey(now - 1) &&
+            data.activities[now - 1] != data.activities[now]
+          ) {
+            subject.sendMessage("${now}点到了！")
+            subject.sendMessage("${data.activities[now - 1]}，完成了吗？")
+            select<Unit> {
+              nextMessageAsync(subject).onAwait {
+                if (it.message.contentToString().contains("没")) {
+                  subject.sendMessage("没法在预定的时间内完成啊")
+                  subject.sendMessage("要延长一小时吗")
+                  if (select<Boolean> {
+                    nextMessageAsync(subject).onAwait {
+                      !it.message.contentToString().contains(Regex("不|算了"))
+                    }
+                    onTimeout(300_000) {
+                      subject.sendMessage("那我就默认为不用了")
+                      false
+                    }
+                  }) {
+                    data.activities[now] = data.activities[now - 1]!!
+                    subject.sendMessageIn("好", "行")
+                    subject.sendMessage("接下来加油哦")
+                  }
+                } else {
                   subject.sendMessageIn(
-                    "休息一下再继续吧",
+                    "那么，休息一下再继续做别的事吧",
                     "继续之前，给自己一点放松时间吧"
                   )
                 }
               }
+              onTimeout(300_000) {
+                subject.sendMessage("还在忙吗……")
+                subject.sendMessage("那不打扰你了")
+              }
             }
-            onTimeout(300_000) {
-              subject.sendMessage("还在忙吗……")
-              subject.sendMessage("那不打扰你了")
-              subject.sendMessage("不过，最好还是休息一下")
+          } else {
+            subject.sendMessageIn(
+              "又一个小时过去了",
+              "${now}点到了！"
+            )
+            subject.sendMessageIn(
+              "这一小时做了什么呢",
+              "在做什么呢？"
+            )
+            select<Unit> {
+              nextMessageAsync(subject).onAwait {
+                val msg = it.message.contentToString().trimStart('在')
+                data.activities[now - 1] = msg
+                val doneTasks = data.tasks.filter { msg.contains(it) }
+                  .onEach { data.tasks.remove(it) }
+                if (doneTasks.isNotEmpty()) {
+                  subject.sendMessage("不错嘛，完成了${doneTasks.size}项目标")
+                  subject.sendMessageIn(
+                    "休息一下再继续吧",
+                    "继续之前，给自己一点放松时间吧"
+                  )
+                  if (data.tasks.isNotEmpty())
+                    subject.sendMessage("接下来还要${data.tasks.joinToString("、")}")
+                  else
+                    subject.sendMessage("这样今天的目标就全都完成啦")
+                } else when {
+                  msg.contains(Regex("色色|打飞机|撸管|手冲")) -> {
+                    subject.sendMessageIn("啧", "嘶")
+                    subject.sendMessage("那不打扰你了")
+                    subject.sendMessage("玩得愉快hhh")
+                  }
+                  msg.contains(Regex("[玩打](游戏|.游|MC|mc|osu)|出去玩")) -> {
+                    subject.sendMessage("玩得开心~")
+                  }
+                  msg.contains(Regex("吃|恰饭")) -> {
+                    subject.sendMessage("好吃吗")
+                  }
+                  msg.contains(Regex("坐车|开车|在路上")) -> {
+                    subject.sendMessage("赶路很辛苦吧")
+                    subject.sendMessage("祝你不虚此行")
+                  }
+                  msg.contains(Regex("午休|休息|睡觉")) -> {
+                    subject.sendMessage("休息得好吗？")
+                    subject.sendMessage("恢复精力之后就继续工作吧")
+                  }
+                  msg.contains(Regex("什么都没做|什么也没做|摸鱼")) -> {
+                    data.activities[now - 1] = "摸鱼"
+                    subject.sendMessageIn("啊这", "这样啊")
+                    subject.sendMessage("那接下来得加油咯")
+                  }
+                  else -> {
+                    subject.sendMessageIn("不错嘛", "可以")
+                    subject.sendMessageIn(
+                      "休息一下再继续吧",
+                      "继续之前，给自己一点放松时间吧"
+                    )
+                  }
+                }
+              }
+              onTimeout(300_000) {
+                subject.sendMessage("还在忙吗……")
+                subject.sendMessage("那不打扰你了")
+                subject.sendMessage("不过，最好还是休息一下")
+              }
             }
           }
         }
@@ -392,27 +441,35 @@ object Alarm: CoroutineScope by Main {
             "一天差不多结束了",
             "已经${data.endOfDay}点了"
           )
-          subject.sendMessageIn(
-            "回顾一下这一天吧……",
-            "看看这一天你做了些什么……"
-          )
-          data.activities.keys.sorted().forEach { time ->
-            subject.sendMessage("${time}点，你在${data.activities[time]}")
-          }
-          subject.sendMessage("${data.endOfDay - 1}点，你在……")
-          select<Unit> {
-            nextMessageAsync(subject).onAwait {
-              data.activities[data.endOfDay - 1] =
-                it.message.contentToString().trimStart('在')
+          if (
+            data.noDisturb &&
+            data.activities.size == 1 &&
+            data.activities.containsKey(now - 1)
+          ) {
+            subject.sendMessage("${data.activities[now - 1]}一整天，感觉怎么样？")
+          } else {
+            subject.sendMessageIn(
+              "回顾一下这一天吧……",
+              "看看这一天你做了些什么……"
+            )
+            data.activities.keys.sorted().forEach { time ->
+              subject.sendMessage("${time}点，你在${data.activities[time]}")
             }
-            onTimeout(300_000) {
-              subject.sendMessage("……算了")
+            subject.sendMessage("${data.endOfDay - 1}点，你在……")
+            select<Unit> {
+              nextMessageAsync(subject).onAwait {
+                data.activities[data.endOfDay - 1] =
+                  it.message.contentToString().trimStart('在')
+              }
+              onTimeout(300_000) {
+                subject.sendMessage("……算了")
+              }
             }
+            subject.sendMessageIn(
+              "感觉怎么样？",
+              "评价一下自己？"
+            )
           }
-          subject.sendMessageIn(
-            "感觉怎么样？",
-            "评价一下自己？"
-          )
           select<Unit> {
             nextMessageAsync(subject).onAwait {
               data.assessment = it.message.contentToString()
@@ -425,10 +482,7 @@ object Alarm: CoroutineScope by Main {
               subject.sendMessage("不管怎样，现在到休息时间了")
             }
           }
-          subject.sendMessageIn(
-            "去洗漱吧",
-            "关掉电脑吧"
-          )
+          subject.sendMessage("晚安")
           data.calendar[LocalDate.now().toString()] =
             UserDay(
               false,
