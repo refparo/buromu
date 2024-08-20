ebab6d23994213438849bf8a8a603a5d414a625a
refparo <ref.paro@outlook.com>
2024-08-21 02:08:52 +0800

    enable onebot 11

--- bot.py
+++ bot.py
@@ -1,10 +1,15 @@
 import nonebot as nb
 from nonebot.adapters.console import Adapter as ConsoleAdapter
+from nonebot.adapters.onebot.v11 import Adapter as OB11Adapter
 
 nb.init()
 
 driver = nb.get_driver()
-driver.register_adapter(ConsoleAdapter)
+
+if driver.env == "prod":
+  driver.register_adapter(OB11Adapter)
+else:
+  driver.register_adapter(ConsoleAdapter)
 
 nb.load_plugins("plugins")
 
